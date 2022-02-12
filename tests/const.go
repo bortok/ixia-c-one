4092829494b04739b9ce7242883c5b9b847f03c6
Ashutosh Kumar <ashutshkumr@gmail.com>
2022-02-12 15:25:08 +0000

    added bgp route install test

---
+++ tests/const.go
@@ -0,0 +1,8 @@
+package tests
+
+const (
+	otgHttpLocation  = "https://clab-ixia-c-ixia-c-one"
+	otgPort1Location = "ixia-c-port-eth1:5555+ixia-c-port-eth1:50071"
+	otgPort2Location = "ixia-c-port-eth2:5555+ixia-c-port-eth2:50071"
+	dutSshLocation   = "clab-ixia-c-ceos:22"
+)
---
+++ tests/helpers/const.go
@@ -0,0 +1,5 @@
+package helpers
+
+const (
+	optsDebug = false
+)
