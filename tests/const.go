23e312968781fa2a5996c901dd6bc608474123fc
Alex Bortok <dev@bortok.com>
2023-02-17 11:14:45 -0800

    8443

--- tests/const.go
+++ tests/const.go
@@ -1,7 +1,7 @@
 package tests
 
 const (
-	otgHttpLocation  = "https://clab-ixia-c-ixia-c-one"
+	otgHttpLocation  = "https://clab-ixia-c-ixia-c-one:8443"
 	otgPort1Location = "eth1"
 	otgPort2Location = "eth2"
 	dutSshLocation   = "clab-ixia-c-ceos:22"
