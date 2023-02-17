325bcd108205f954b25fb4134f2d977a899aebb9
Alex Bortok <dev@bortok.com>
2023-02-17 11:09:29 -0800

    keysight_ixia-c-one

--- tests/const.go
+++ tests/const.go
@@ -2,7 +2,7 @@ package tests
 
 const (
 	otgHttpLocation  = "https://clab-ixia-c-ixia-c-one"
-	otgPort1Location = "ixia-c-port-eth1:5555+ixia-c-port-eth1:50071"
-	otgPort2Location = "ixia-c-port-eth2:5555+ixia-c-port-eth2:50071"
+	otgPort1Location = "eth1"
+	otgPort2Location = "eth2"
 	dutSshLocation   = "clab-ixia-c-ceos:22"
 )
